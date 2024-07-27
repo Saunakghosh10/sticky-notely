dab4c2165e0f5a3c0205f4e5e196def31b538b5f
saunak10 <saunakghosh710@gmail.com>
2024-07-27 13:00:14 +0530

    full app done

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,47 +1,83 @@
 import Trash from "../icons/Trash";
 import { useRef, useEffect } from "react";
-import { setNewOffset, autoGrow, setZIndex } from "../utils.js";
-
+import DeleteButton from "./DeleteButton.jsx";
+import { setNewOffset, autoGrow, setZIndex, bodyParser } from "../utils.js";
+import { db } from "../appwrite/databases.js";
+import Spinner from "../icons/Spinner.jsx";
 import { useState } from "react";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext.jsx";
 const NoteCard = ({ note }) => {
-  const colors = JSON.parse(note.colors);
+  const colors = bodyParser(note.colors);
+  const [saving, setSaving] = useState(false);
+  const keyUpTimer = useRef(null);
   const body = JSON.parse(note.body);
   const textAreaRef = useRef(null);
   const [position, setPosition] = useState(JSON.parse(note.position));
+  const { setSelectedNote } = useContext(NoteContext);
 
   useEffect(() => {
     autoGrow(textAreaRef);
+    setZIndex(cardRef.current);
   }, []);
 
   let mouseStartPos = { x: 0, y: 0 };
 
   const cardRef = useRef(null);
   const mouseDown = (e) => {
-    setZIndex(cardRef.current);
-    mouseStartPos.x = e.clientX;
-    mouseStartPos.y = e.clientY;
+    if (e.target.className === "card-header") {
+      setZIndex(cardRef.current);
+      setSelectedNote(note);
 
-    document.addEventListener("mousemove", mouseMove);
-    document.addEventListener("mouseup", mouseUp);
+      mouseStartPos.x = e.clientX;
+      mouseStartPos.y = e.clientY;
+
+      document.addEventListener("mousemove", mouseMove);
+      document.addEventListener("mouseup", mouseUp);
+    }
   };
   const mouseMove = (e) => {
-    //1 - Calculate move direction
     let mouseMoveDir = {
       x: mouseStartPos.x - e.clientX,
       y: mouseStartPos.y - e.clientY,
     };
 
-    //2 - Update start position for next move.
     mouseStartPos.x = e.clientX;
     mouseStartPos.y = e.clientY;
     const newPosition = setNewOffset(cardRef.current, mouseMoveDir);
     setPosition(newPosition);
-    //3 - Update card top and left position.
+
     setPosition(newPosition);
   };
   const mouseUp = () => {
     document.removeEventListener("mousemove", mouseMove);
     document.removeEventListener("mouseup", mouseUp);
+    const newPosition = setNewOffset(cardRef.current); //{x,y}
+    saveData("position", newPosition);
+  };
+  const saveData = async (key, value) => {
+    const payload = { [key]: JSON.stringify(value) };
+    try {
+      await db.notes.update(note.$id, payload);
+    } catch (error) {
+      console.error(error);
+    }
+    setSaving(false);
+  };
+
+  const handleKeyUp = async () => {
+    //1 - Initiate "saving" state
+    setSaving(true);
+
+    //2 - If we have a timer id, clear it so we can add another two seconds
+    if (keyUpTimer.current) {
+      clearTimeout(keyUpTimer.current);
+    }
+
+    //3 - Set timer to trigger save in 2 seconds
+    keyUpTimer.current = setTimeout(() => {
+      saveData("body", textAreaRef.current.value);
+    }, 2000);
   };
   return (
     <div
@@ -58,12 +94,20 @@ const NoteCard = ({ note }) => {
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
       >
-        <Trash />
+        <DeleteButton noteId={note.$id} />
+        {saving && (
+          <div className="card-saving">
+            <Spinner color={colors.colorText} />
+            <span style={{ color: colors.colorText }}>Saving...</span>
+          </div>
+        )}
       </div>
       <div className="card-body">
         <textarea
+          onKeyUp={handleKeyUp}
           onFocus={() => {
             setZIndex(cardRef.current);
+            setSelectedNote(note);
           }}
           ref={textAreaRef}
           style={{ color: colors.colorText }}
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,12 +1,16 @@
-import { fakeData as notes } from "../assets/fakeData.js";
 import NoteCard from "../components/NoteCard";
-
+import { NoteContext } from "../context/NoteContext";
+import { useContext } from "react";
+import Controls from "../components/Controls";
 const NotesPage = () => {
+  const { notes, setNotes } = useContext(NoteContext);
+
   return (
     <div>
       {notes.map((note) => (
-        <NoteCard note={note} key={note.$id} />
+        <NoteCard note={note} setNotes={setNotes} key={note.$id} />
       ))}
+      <Controls />
     </div>
   );
 };
